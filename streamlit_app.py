0b8cd104f807ab783e4ee9a597d8e66aa88363c1
TMarszalek23 <83115127+TMarszalek23@users.noreply.github.com>
2025-05-17 13:25:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import fitz  # PyMuPDF
 import os
 import chat_openrouter
 import embedder
-import Dockerfile
+#import Dockerfile
 
 st.set_page_config(layout="wide", page_title="OpenRouter chatbot app")
 st.title("OpenRouter chatbot app")
